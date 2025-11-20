08570d15427fd13a98691d318cc181a39a558c05
Luis Redigolo <luisguirc@gmail.com>
2025-11-19 22:01:16 -0300

    feat: generators and prefab everything

---
+++ Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class WinTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GameManager.Instance.GameWin("You activated the generator!");
+        }
+    }
+}
