479fecd4cef8b7163b4387b829ca764911a64f02
Luis Redigolo <luisguirc@gmail.com>
2025-11-19 22:16:21 -0300

    feat: pause screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,9 +13,22 @@ public class GameManager : MonoBehaviour
     public GameObject gameWinUI;
     public TextMeshProUGUI winReasonText;
 
+    [Header("Pause UI")]
+    public GameObject pauseUI;
+
+    private bool isPaused = false;
+
     void Awake()
     {
         Instance = this;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            this.TogglePause();
+        }
     }
 
     public void GameOver(string reason)
@@ -40,16 +53,42 @@ public class GameManager : MonoBehaviour
             gameWinUI.SetActive(true);
     }
 
+    public void TogglePause()
+    {
+        if (isPaused)
+            Resume();
+        else
+            Pause();
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        isPaused = true;
+
+        if (pauseUI != null)
+            pauseUI.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        isPaused = false;
+
+        if (pauseUI != null)
+            pauseUI.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
     public void Restart()
     {
         Time.timeScale = 1f;
         UnityEngine.SceneManagement.SceneManager.LoadScene(
             UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex
         );
-    }
-
-    public void Quit()
-    {
-        Application.Quit();
     }
 }
